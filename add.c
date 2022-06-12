cd11375116aa18319b86ccbe80cb66872852c9fc
Nicolas Mercado <nicolasmercado452@gmail.com>
2022-06-12 02:49:28 -0300

    Se agregan TODOs

--- add.c
+++ add.c
@@ -6,7 +6,7 @@
 //IMPLEMENTACION
 void add(char *path){
   FILE *file;
- 
+  //TODO: Comprobar que el string 'path' sea una ruta verdadera y existente.
   file = fopen("paths.txt", "a");
   if(file == NULL) EXIT_FAILURE;
 	fputs(path, file);fputs("\n",file);
