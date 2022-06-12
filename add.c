0788e97c2ab665b9f8857275a39b92720b9943cf
Nicolas Mercado <nicolasmercado452@gmail.com>
2022-06-12 02:07:57 -0300

    Se implementa y prueba la funcion add()
    Se implementa y prueba la función add() de manera independiente sin integrarla al archivo principal (backup.c)

--- add.c
+++ add.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
-#include "add.h"
+#include <string.h>
+#include <stdlib.h>
 
+#include "add.h"
+#define MAX 100
 //IMPLEMENTACION
-void add(){
-  puts("es add"); 
+int main() {
+  char p[MAX] = "";
+  printf("Ingrese un Path: ");
+  // scanf("%s", p);
+  fgets(p, sizeof p, stdin);
+  printf("El Path es: %s\n", p);
+  puts("antes de funcion add\n");
+  add(p);
+  printf("\nSe guardo en el archivo de configuracion\n");
+  return 0;
+}
+void add(char *path){
+  puts("es add");
+  FILE *file;
+ 
+  file = fopen("path.txt", "a");
+  puts("Abrio el archivo\n");
+  if(file == NULL) EXIT_FAILURE;
+  printf("%s\n", path);
+  puts("Controlo el null\n");
+	fputs(path, file);
+  puts("Escribio el archivo\n");
+  fflush(file);
+  fclose(file);
+  puts("Cerro el archivo\n");
 }
