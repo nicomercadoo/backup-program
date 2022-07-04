4c99b1936c9aac48233fd5a4e8f2eba9a1a245de
Nicolas Mercado <nicolasmercado452@gmail.com>
2022-07-03 22:34:50 -0300

    Se corrige el manejo de errores.
    Se corrige el manejo de errores y se hace un refactor.
    Ademas: Se agregan achivos a .gitignore

--- add.c
+++ add.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
 
 #include "add.h"
 
-//IMPLEMENTACION
-void add(char *path){
+void add(char *argpath){
   FILE *file;
+  char buf[PATH_MAX];
+  char *path;
+  
+  if (argpath == NULL){
+    printf("Faltan Argumentos.\n");
+    exit(EXIT_FAILURE);
+  } else {
+    path = realpath(argpath, buf);
+    file = fopen("paths.txt", "a");
+
+    if (file == NULL) {
+      printf("No se encontro o no se pudo abrir el archivo paths.txt.\n");
+      exit(EXIT_FAILURE);
+    }
+    fputs(path, file);fputs("\n",file);
+    fflush(file);
+    fclose(file);
+  };
   //TODO: Comprobar que el string 'path' sea una ruta verdadera y existente.
-  file = fopen("paths.txt", "a");
-  if(file == NULL) EXIT_FAILURE;
-	fputs(path, file);fputs("\n",file);
-  fflush(file);
-  fclose(file);
 }
