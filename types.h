7a6c37a24e89d776cf09c70927239f65f874898a
Nicolas Mercado <nicolasmercado452@gmail.com>
2022-07-05 02:46:34 -0300

    Se definen colores
    Se definen colores para trabajar de una mejor manera y hacer más
    entendibles las impresiones por pantalla.

--- types.h
+++ types.h
@@ -1,7 +1,15 @@
-#pragma once
-
 #define MAX 300
 #define PATHS_FILE "paths.txt"
+
+//Colors
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define YEL   "\x1B[33m"
+#define BLU   "\x1B[34m"
+#define MAG   "\x1B[35m"
+#define CYN   "\x1B[36m"
+#define WHT   "\x1B[37m"
+#define RESET "\x1B[0m"
 
 typedef enum {
   LEAVE,
